b206f82eb0174cb51026ba694f9d0799510c2263
Anicet Togue <atogue@gmail.com>
2021-01-19 19:38:00 +0100

    init project

---
+++ src/main/java/org/craftchain/market/config/MarketConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/craftchain/market/config/MarketConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
